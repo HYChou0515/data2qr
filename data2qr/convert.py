2f9310be4f049251fa7c766644cc400946bb1746
Chou Hung-Yi <s1243221@gmail.com>
2021-07-18 16:21:59 +0800

    add support to vedio

--- data2qr/convert.py
+++ data2qr/convert.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
 import bz2
+import cv2
 import qrcode
 from PIL import Image
 from pyzbar import pyzbar
-from joblib import Parallel, delayed
 import math
 
-__all__ = ['data2code', 'code2qrcode', 'qrcode2code', 'code2data']
+__all__ = ['data2code', 'code2qrcode', 'qrcode2code', 'qrcode2code_v2', 'code2data']
 
 """
 This program effectively encodes and transforms data to QR code.
@@ -32,6 +32,8 @@ D.extend([chr(ord('A')+i) for i in range(26)])
 D.extend([' ', '$', '%', '*', '+', '-', '.', '/', ':'])
 D = np.array(D)
 IDX_D = {D[i]: i for i in range(len(D))}
+CODE_SEP1 = '@'
+CODE_SEP2 = '#'
 
 QRCODE_MAX_DICT={
         qrcode.constants.ERROR_CORRECT_L: 1852,
@@ -40,26 +42,44 @@ QRCODE_MAX_DICT={
         qrcode.constants.ERROR_CORRECT_H: 490,
 }
 
-def _default_image_name_generator(nr_imgs, prefix=None):
-    fmt = '%%0%dd.png' % (int(math.floor(math.log10(nr_imgs)))+1)
+def _default_image_name_generator(prefix=None):
+    fmt = '%d.png'
     if prefix is not None:
         fmt = prefix + fmt
-    for i in range(nr_imgs):
+    i = 0
+    while True:
         yield fmt % i
+        i += 1
 
-def data2code(s):
-    byte_array=bz2.compress(s)
-    bits = ''.join(map(lambda bt: bin(bt)[2:].zfill(8), byte_array))
+def _bits_to_code(bits):
     bits = bits.zfill(len(bits) + (-len(bits)%5)) # padding zero so len(bits) % 5 == 0
     np_num32 = [int(bits[i:i+5], 2) for i in range(0, len(bits), 5)]
     encoded = ''.join(D[np_num32])
     return encoded
 
-def code2qrcode(encoded, image_name_generator=None, prefix=None, n_jobs=1):
+def data2code(s):
+    byte_array=bz2.compress(s)
+    bits = ''.join(map(lambda bt: bin(bt)[2:].zfill(8), byte_array))
+    return _bits_to_code(bits)
+
+def code2qrcode(encoded, image_name_generator=None, prefix=None, prepend_idx=False, n_jobs=1):
     ERROR_CORRECT = qrcode.constants.ERROR_CORRECT_M
     QRCODE_MAX = QRCODE_MAX_DICT[ERROR_CORRECT]
-    chunks = [encoded[i:i+QRCODE_MAX] for i in range(0, len(encoded), QRCODE_MAX)]
-    image_names =image_name_generator if image_name_generator is not None else _default_image_name_generator(len(chunks), prefix=prefix)
+    if prepend_idx:
+        def prepended_chunk(encoded):
+            st = 0
+            id_chunk = 0
+            while st < len(encoded):
+                encoded_id = _bits_to_code(bin(id_chunk)[2:])
+                ed = st + QRCODE_MAX - len(encoded_id) - 1
+                code_sep = CODE_SEP1 if ed < len(encoded) else CODE_SEP2
+                yield encoded_id + code_sep + encoded[st:ed]
+                st = ed
+                id_chunk += 1
+        chunks = prepended_chunk(encoded)
+    else:
+        chunks = (encoded[i:i+QRCODE_MAX] for i in range(0, len(encoded), QRCODE_MAX))
+    image_names =image_name_generator if image_name_generator is not None else _default_image_name_generator(prefix=prefix)
 
     def to_qrcode_1(_chunk, _image_name, _i):
         qr = qrcode.QRCode(error_correction=ERROR_CORRECT)
@@ -67,25 +87,76 @@ def code2qrcode(encoded, image_name_generator=None, prefix=None, n_jobs=1):
         if os.path.exists(_image_name):
             raise os.error(f'File "{_image_name}" exists')
         qr.make_image().save(_image_name, 'png')
-        info(f'{_i}/{len(chunks)}')
+        info(f'{_i}')
+        return _image_name
 
-    image_names = [image_name for image_name in image_names][:len(chunks)]
-    Parallel(n_jobs=n_jobs, prefer='processes')(
-        delayed(to_qrcode_1)(chunk, image_name, i)
-        for i, (chunk, image_name) in enumerate(zip(chunks, image_names))
-    )
+    image_names = [to_qrcode_1(chunk, image_name, i) for i, (chunk, image_name) in enumerate(zip(chunks, image_names))]
+#    image_names = Parallel(n_jobs=n_jobs, prefer='processes')(
+#        delayed(to_qrcode_1)(chunk, image_name, i)
+#        for i, (chunk, image_name) in enumerate(zip(chunks, image_names))
+#    )
     return image_names
+
+def qrcode2code_v2(frame, quite_mode=True):
+    decoded_objs = pyzbar.decode(frame)
+    qr_str = None
+
+    # only take one frame
+    if len(decoded_objs) > 0:
+        decoded_obj = decoded_objs[0]
+
+        qr_str = decoded_obj.data.decode('utf-8')
+        if not quite_mode:
+            # mark qrcode in rectangle
+            rect = decoded_obj.rect
+            points = [(rect.left, rect.top), (rect.left, rect.top+rect.height),
+                    (rect.left+rect.width, rect.top+rect.height), (rect.left+rect.width, rect.top)]
+
+            if len(points) > 4 :
+                hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+                hull = list(map(tuple, np.squeeze(hull)))
+            else :
+                hull = points;
+
+            # Number of points in the convex hull
+            n = len(hull)
+
+            # Draw the convext hull
+            for j in range(0,n):
+                cv2.line(frame, hull[j], hull[ (j+1) % n], (255,0,0), 3)
+
+    if quiet_mode:
+        return qr_str
+    else:
+        return qr_str, frame
 
 def qrcode2code(img_name):
     img = Image.open(img_name)
     data = pyzbar.decode(img)
     return data[0].data.decode('utf-8')
 
-def code2data(encoded):
+def code2data(encoded, encode_method='bz2', prepend_idx=False):
     encoded = encoded.replace('\n','')
-    num32_arr = [IDX_D[c] for c in encoded]
-    bits = ''.join([bin(x)[2:].zfill(5) for x in num32_arr])
-    bits = bits[len(bits)%8:]  # remove padding
-    bit8_array = [bits[i:i+8] for i in range(0, len(bits), 8)]
-    return bz2.decompress(bytes([int(byte, 2) for byte in bit8_array]))
+    if prepend_idx:
+        if CODE_SEP1 in encoded:
+            idx_ed = encoded.index(CODE_SEP1)
+            last_chunk = False
+        elif CODE_SEP2 in encoded:
+            idx_ed = encoded.index(CODE_SEP2)
+            last_chunk = True
+        else:
+            raise Exception(f'{CODE_SEP1} and {CODE_SEP2} are not in encoded.')
+        chunk_id = code2data(encoded[:idx_ed], encode_method='int')
+        return chunk_id, encoded[idx_ed+1:], last_chunk
+    else:
+        num32_arr = [IDX_D[c] for c in encoded]
+        bits = ''.join([bin(x)[2:].zfill(5) for x in num32_arr])
+        if encode_method == 'bz2':
+            bits = bits[len(bits)%8:]  # remove padding
+            bit8_array = [bits[i:i+8] for i in range(0, len(bits), 8)]
+            return bz2.decompress(bytes([int(byte, 2) for byte in bit8_array]))
+        elif encode_method == 'int':
+            return int(bits, 2)
+        else:
+            raise NotImplemented
 
--- try_cv.py
+++ try_cv.py
@@ -37,31 +37,33 @@ def capture_video():
     out.release()
     cv2.destroyAllWindows()
 
-def scan_qr(frame):
+def scan_qr(frame, quite_mode):
     decoded_objs = pyzbar.decode(frame)
 
     qr_str = None
-    for decoded_obj in decoded_objs:
+
+    # only take one frame
+    if len(decoded_objs) > 0:
+        decoded_obj = decoded_objs[0]
+
         qr_str = decoded_obj.data.decode('utf-8')
-        rect = decoded_obj.rect
-        points = [(rect.left, rect.top), (rect.left, rect.top+rect.height),
-                (rect.left+rect.width, rect.top+rect.height), (rect.left+rect.width, rect.top)]
+        if not quite_mode:
+            rect = decoded_obj.rect
+            points = [(rect.left, rect.top), (rect.left, rect.top+rect.height),
+                    (rect.left+rect.width, rect.top+rect.height), (rect.left+rect.width, rect.top)]
 
-        if len(points) > 4 :
-            hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
-            hull = list(map(tuple, np.squeeze(hull)))
-        else :
-            hull = points;
+            if len(points) > 4 :
+                hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+                hull = list(map(tuple, np.squeeze(hull)))
+            else :
+                hull = points;
 
-        # Number of points in the convex hull
-        n = len(hull)
+            # Number of points in the convex hull
+            n = len(hull)
 
-        # Draw the convext hull
-        for j in range(0,n):
-            cv2.line(frame, hull[j], hull[ (j+1) % n], (255,0,0), 3)
-
-        # only take one frame
-        break
+            # Draw the convext hull
+            for j in range(0,n):
+                cv2.line(frame, hull[j], hull[ (j+1) % n], (255,0,0), 3)
 
     return frame, qr_str
 
@@ -79,19 +81,21 @@ def analysis_video(vname, scan_qr_rate=10, quite_mode=False):
 
     state = {'paused': False, 'scan_qr': scan_qr_rate}
 
-    qr_strings = []
-    last_qr_str = None
+    qr_strings = {}
+    nr_chunks = None
     while cap.isOpened():
         if not state['paused']:
             ret, frame = cap.read()
         if ret:
             if state['scan_qr'] <= 0:
-                frame, qr_str = scan_qr(frame)
+                frame, qr_str = scan_qr(frame, quite_mode)
                 if qr_str is not None:
                     state['scan_qr'] = scan_qr_rate
-                    if last_qr_str is None or last_qr_str != qr_str:
-                        qr_strings.append(qr_str)
-                        last_qr_str = qr_str
+                    if qr_str not in qr_strings:
+                        chunk_id, data, last_chunk = code2data(qr_str, prepend_idx=True)
+                        qr_strings[qr_str] = chunk_id, data
+                        if last_chunk:
+                            nr_chunks = chunk_id + 1
             else:
                 state['scan_qr'] -= 1
 
@@ -111,10 +115,22 @@ def analysis_video(vname, scan_qr_rate=10, quite_mode=False):
                     print('Paused')
         else:
             break
-    return ''.join(qr_strings)
+    if nr_chunks is None:
+        raise Exception('Cannot get nr_chunks')
+    if len(qr_strings) != nr_chunks:
+        raise Exception(f'got {len(qr_strings)} qrcodes but should have {nr_chunks}')
+    qs_list = [None for _ in range(nr_chunks)]
+    for chunk_id, data in qr_strings.values():
+        if chunk_id < 0 or chunk_id >= nr_chunks:
+            raise Exception(f'got chunk_id={chunk_id} but should between 0 and {nr_chunks-1}')
+        if qs_list[chunk_id] is not None:
+            raise Exception(f'got multiple chunk_id={chunk_id} that have different data')
+        qs_list[chunk_id] = data
+    return code2data(''.join(qs_list))
 
 if __name__ == '__main__':
     import sys
     #capture_video()
-    decoded_str = analysis_video(sys.argv[1], quite_mode=True)
+    decoded_str = analysis_video(sys.argv[1], quite_mode=False)
     print(decoded_str)
+
